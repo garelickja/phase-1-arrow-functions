97da9a86ebcb9bf54fd60f154bd8ddceda4eaf33
garelickja <138186599+garelickja@users.noreply.github.com>
2024-01-30 15:23:31 -0600

    arrow

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(a='2000', b='100'){
+    return a/b;
+}
+
+const square = x => x ** 2;
+
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`)
+    return a + b;
+}
